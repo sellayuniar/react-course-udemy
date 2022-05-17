1fef8972fb1962cc07c2a10bf6c4f7149b16b114
sellayuniar <synrr212@gmail.com>
2022-05-17 22:35:01 +0800

    Updating state bason on previous state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -14,24 +14,27 @@ const ExpenseForm = () => {
 
     const titleChangeHandler = (event) => {
         // setEnteredTitle(event.target.value);
-        setUserInput({
-            ...userInput,
-            enteredTitle: event.target.value,
-        })
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle: event.target.value,
+        // });
+        setUserInput((prevState) => {
+            return { ...prevState, enteredTitle: event.target.value};
+        });
     };
 
     const amountChangeHandler = (event) => {
         setUserInput({
             ...userInput,
             enteredAmount: event.target.value,
-        })
+        });
     }
 
     const dateChangeHandler = (event) => {
         setUserInput({
             ...userInput,
             enteredDate: event.target.value,
-        })
+        });
     }
 
 
