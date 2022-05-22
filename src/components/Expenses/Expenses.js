a83b60711fe0943700e4abda0eedf0f3e3341421
sellayuniar <synrr212@gmail.com>
2022-05-22 14:39:31 +0800

    Understanding keys

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -18,6 +18,7 @@ function Expenses(props) {
     <ExpensesFilter selected={filterYear} onChangeFilter={filterChangeHandler} />
       {props.items.map((expense) => (
         <ExpenseItem 
+          key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
